405925209adcc65532296bf0ef539239640ce562
DonCastillo <don.qcastillo@yahoo.com>
2021-03-12 02:53:42 -0700

    working toggle button

--- src/components/ToggleColor.js
+++ src/components/ToggleColor.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Container, Switch } from "@material-ui/core";
 
-const ToggleColor = () => {
-    return (
-        <p>Toggle</p>
-    )
-}
+const ToggleColor = (props) => {
+	const [state, setState] = useState(false);
+
+	const changeHandler = (event) => {
+		setState(event.target.checked);
+	};
+
+	useEffect(() => {
+		if (state) {
+			props.toggleColor({ color: "#FFF", backgroundColor: "#000" });
+		} else {
+			props.toggleColor({ color: "#000", backgroundColor: "#FFF" });
+		}
+	}, [state]);
+
+	return (
+		<Container>
+			<Switch
+				value=""
+				checked={state}
+				onChange={changeHandler}
+				inputProps={{ "aria-label": "" }}
+			/>
+		</Container>
+		// <div>
+		//     <p>Toggle</p>
+		// </div>
+	);
+};
 
 export default ToggleColor;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,33 @@
-import "./App.css";
+// import "./App.css";
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import ToggleColor from "./../components/ToggleColor";
 import Markdown from "./../components/Markdown";
 import Html from "./../components/Html";
+import Container from "@material-ui/core/Container";
 const marked = require("marked");
 
-const initialContent = 
-`# Heading 1
+const initialContent = `# Heading 1
 ## Heading 2 
 ### Heading 3`;
-
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			theme: {
+			style: {
 				color: "#FFF",
-				background: "#000",
+				backgroundColor: "#000",
 			},
 			markdown: "",
 			html: "",
 		};
 	}
 
-	toggleColor = () => {
-		this.setState((state) => {
-			const color = state.theme.color === "#000" ? "#FFF" : "#000";
-			const background = state.theme.background === "#000" ? "#FFF" : "#000";
-			return { theme: { color: color, background: background } };
-		});
-	};
+
+	toggleColor = (newTheme) => {
+		this.setState({style: {...newTheme}});
+	}
 
 	setMarkdown = (string) => {
 		this.setState({ markdown: string });
@@ -43,15 +39,14 @@ class App extends React.Component {
 	};
 
 	renderMarkup = (string) => {
-		const el = document.getElementById('html');
+		const el = document.getElementById("html");
 		el.innerHTML = string;
-	}
+	};
 
 	changeHandler = (event) => {
 		this.setMarkdown(event.target.value);
 		this.renderHTML(event.target.value);
 	};
-	
 
 	componentDidMount = () => {
 		this.toggleColor();
@@ -61,19 +56,23 @@ class App extends React.Component {
 
 	componentDidUpdate = () => {
 		this.renderMarkup(this.state.html);
-	}
+	};
 
 	render() {
 		return (
-			<div>
-				<ToggleColor />
+			<Container
+				component="div"
+				maxWidth="xl"
+				style={this.state.style}
+			>
+				<ToggleColor toggleColor={this.toggleColor} />
 				<div id="parent-container">
 					<Markdown change={this.changeHandler}>
 						{this.state.markdown}
 					</Markdown>
 					<Html></Html>
 				</div>
-			</div>
+			</Container>
 		);
 	}
 }
